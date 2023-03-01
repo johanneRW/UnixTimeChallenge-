1e763fd38a33b4fa3d7979b3533b50951f7d20f1
johanne <joha786s@stud.kea.dk>
2023-03-01 22:40:14 +0100

    set mime type

--- time_api/time_app.js
+++ time_api/time_app.js
@@ -1,23 +1,25 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.use(express.static("public"))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
+express.static.mime.define({'text/css': ['css']});
+express.static.mime.define({'text/javascript': ['js']})
 
-app.get("/time", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/time_page.html")
 })
 
 app.listen(8080, (error) => {
     if (error) {
-        console.log
+        console.log(error)
         return
     }
     console.log("Server is running on port", 8080)
 })
 
-module.exports = app;
+module.exports = app
 
